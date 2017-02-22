ee3c910ccb31547dc22fac4703b0956c887eaa48
Paul Blischak <paul.blischak@gmail.com>
2017-02-22 17:12:05 -0500

    changing parallelization to dynamic; accounts for different sample sizes

--- src/HyDe.cpp
+++ src/HyDe.cpp
@@ -69,8 +69,7 @@ void HyDe::run(){
     _outStream << "P1\tHybrid\tP2\tZscore\tPvalue\tX1\tX2\tX3\tX4\tX5\tX6\tX7\tX8\tX9\tX10\tX11\tX12\tX13\tX14\tX15" << std::endl;
   }
 
-  int _chunk = std::ceil((_nTaxa - 1) / _threads);
-  #pragma omp parallel for num_threads(_threads) schedule(dynamic, _chunk) firstprivate(_counts1, _counts2, _counts3, _taxaMap, _taxaNames, _dnaMatrix, _baseLookup, _outgroup, _numObs, _zVals, _pVals)
+  #pragma omp parallel for num_threads(_threads) schedule(dynamic) firstprivate(_counts1, _counts2, _counts3, _taxaMap, _taxaNames, _dnaMatrix, _baseLookup, _outgroup, _numObs, _zVals, _pVals)
   for(unsigned i = 0; i < _taxaNames.size() - 2; i++){
     for(unsigned j = i + 1; j < _taxaNames.size() - 1; j++){
       for(unsigned k = j + 1; k < _taxaNames.size(); k++){
