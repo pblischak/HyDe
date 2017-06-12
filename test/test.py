3ead911f02c163ac523d9553372805e22e240be7
Paul Blischak <blischak.4@osu.edu>
2017-06-12 18:36:18 -0400

    fixing bootReps error in test.py

--- test/test.py
+++ test/test.py
@@ -2,23 +2,23 @@ from __future__ import print_function
 import hyde as hd
 import sys
 
-print("**** Test 1: Data import...", end='')
+print("\n**** Test 1: Data import. ****")
 data = hd.HydeData("data.txt", "map.txt", "out", 16, 4, 50000)
-print("Good. ****")
+print("**** Good. ****")
 
-print("**** Test 2: Running test_triple()...", end='')
+print("\n**** Test 2: Running test_triple(). ****")
 data.test_triple('sp1', 'sp2', 'sp3')
-print("Good. ****")
+print("**** Good. ****")
 
-print("**** Test 3: Running hyde analysis with bootstrapping...", end='')
-hd.run_hyde("data.txt", "map.txt", "out", 16, 4, 50000, bootRep=20)
-print("Good. ****")
+print("\n**** Test 3: Running hyde analysis with bootstrapping. ****")
+hd.run_hyde("data.txt", "map.txt", "out", 16, 4, 50000, bootReps=20)
+print("**** Good. ****")
 
-print("**** Test 4: Reading in bootstrap reps...", end='')
+print("\n**** Test 4: Reading in bootstrap reps. ****")
 boots = hd.Bootstrap("hyde-boot.txt")
-print("Good. ****")
+print("**** Good. ****")
 
-print("**** Test 5: Getting gamma values from each boot rep...", end='')
+print("\n**** Test 5: Getting gamma values from each boot rep. ****")
 g = boots.gammas()
 print(g)
-print("Good. ****")
+print("**** Good. ****")
