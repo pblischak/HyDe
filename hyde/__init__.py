554ab3e40bbfd001fec2df245e07764b9106d1d4
Paul Blischak <blischak.4@osu.edu>
2017-06-15 17:03:34 -0400

    cleaning up after name change

--- hyde/__init__.py
+++
@@ -1,9 +0,0 @@
-
-__version__ = "0.1.0"
-__author__  = "Paul Blischak and Laura Kubatko"
-
-from hyde.analyze.main import run_hyde
-from hyde.core.bootstrap import Bootstrap
-from hyde.core.data import HydeData
-from hyde.core.result import HydeResult
-from hyde.visualize import viz
