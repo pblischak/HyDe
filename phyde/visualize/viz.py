90a3f412f5e4fa89aeb76299ebf48d37ebb865b1
Paul Blischak <paul.blischak@gmail.com>
2018-01-26 16:24:26 -0500

    fixing title in plot commands

--- phyde/visualize/viz.py
+++ phyde/visualize/viz.py
@@ -35,7 +35,7 @@ def density(boot_obj, attr, p1, hyb, p2, title="", xlab="", ylab="", shade=True,
     sns.set_style("white")
     ax = sns.kdeplot(np.array(boot_obj(attr, p1, hyb, p2)), shade=shade, color=color)
     ax.set(ylabel=ylab, xlabel=xlab)
-    sns.plt.title(title)
+    plt.title(title)
     return ax
 
 def dist(boot_obj, attr, p1, hyb, p2, title="", xlab="", ylab="", hist=True, color='b'):
@@ -67,7 +67,7 @@ def dist(boot_obj, attr, p1, hyb, p2, title="", xlab="", ylab="", hist=True, col
     sns.set_style("white")
     ax = sns.distplot(np.array(boot_obj(attr, p1, hyb, p2)), hist=hist, color=color)
     ax.set(ylabel=ylab, xlabel=xlab)
-    sns.plt.title(title)
+    plt.title(title)
     return ax
 
 def violinplot(boot_obj, attr, p1, hyb, p2, title="", xlab="", ylab="", color='b'):
@@ -97,5 +97,5 @@ def violinplot(boot_obj, attr, p1, hyb, p2, title="", xlab="", ylab="", color='b
     sns.set_style("white")
     ax = sns.violinplot(np.array(boot_obj(attr, p1, hyb, p2)), color=color)
     ax.set(ylabel=ylab, xlabel=xlab)
-    sns.plt.title(title)
+    plt.title(title)
     return ax
